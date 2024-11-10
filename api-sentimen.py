3cb3bbeecab0ed29458cc91419dd5d37b3a368e7
eringnwn <eringnwn@gmail.com>
2024-11-10 22:02:39 +0700

    [feat] add additional negative handling (sync from notebook)

--- api-sentimen.py
+++ api-sentimen.py
@@ -46,10 +46,15 @@ def handle_negative(review):
       negative_review.append(word)
     else:
       negative_review.pop()
-      word = 'tidak_'+word
+      word = 'tidak_' + word
+      if (i != len(review)-1) and (review[i+1] == 'buruk' or review[i+1] == 'jelek'):
+        word = word + '_' + review[i+1]
+        i += 1 #increase index agar kata 'buruk' dan 'jelek' tidak di-consider lagi
+      if (i < len(review)-2) and (review[i+1] == "efek" and (review[i+2] == 'buruk' or review[i+2] == 'jelek')):
+        word = 'tidak_'+ review[i+1] + '_' + review[i+2]
+        i += 2 #increase index agar kata 'efek', 'buruk' dan 'jelek' tidak di-consider lagi
       negative_review.append(word)
   for_stemming = ' '.join(negative_review)
-  return for_stemming
 
 # Remove emoji, punctuation, symbol
 def preprocess(text):
@@ -57,7 +62,11 @@ def preprocess(text):
   text = text.lower()
 
   # Remove punctuation
-  text = text.translate(str.maketrans('', '', string.punctuation))
+  text = text.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation))) #ubah tanda baca jadi spasi dulu
+  text = ' '.join(text.split()) #lalu hapus multiple spasi jadi satu spasi
+  # kenapa begini? karena ini teks review, beberapa orang membuat ulasan dengan tanda baca yang tidak rapi.
+  # misalnya "bagusbaget.rekomen" kalau langsung dihapus titiknya tanpa kasih spasi dulu, ntar kalimatnya jadi aneh
+  text = re.sub(r'[^\x00-\x7F]+', ' ', text) #remove non-ascii character (misalnya emoji). [^\x00-\x7F] artinya hanya ambil yang ascii-nya dari 0 sd 127
 
   text = replace_word_elongation(text)  # replace WE
 
